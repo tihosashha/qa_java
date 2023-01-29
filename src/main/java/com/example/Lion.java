fb6649ca9ef3f2a483d003dcd178424f30c89793
Aleksandra Dombaeva <tiho.sasha33@yandex.com>
2023-01-29 20:02:22 +0100

    Revert "Add tests and reports"
    This reverts commit 8df567363990414a7a4b9772eb671ecfb478455a.

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,9 +5,8 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
-    Feline feline;
-    public Lion(String sex, Feline feline) throws Exception {
-        this.feline = feline;
+
+    public Lion(String sex) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -17,7 +16,7 @@ public class Lion {
         }
     }
 
-
+    Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
