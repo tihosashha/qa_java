e8a4f69509fa376c4a63a38422262723f2ad2b5e
Aleksandra Dombaeva <tiho.sasha33@yandex.com>
2023-01-29 20:11:28 +0100

    Git fixes

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,4 +1,31 @@
-import junit.framework.TestCase;
-public class CatTest extends TestCase {
-  
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class CatTest {
+    @Mock
+    Feline feline;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void whenGetSoundThanReturnMeow(){
+        Cat cat = new Cat(feline);
+        Assert.assertEquals("Мяу", cat.getSound());
+    }
+    @Test
+    public void whenGetFoodThanCallEatMeat() throws Exception{
+        Cat cat = new Cat(feline);
+
+        cat.getFood();
+
+        Mockito.verify(feline).eatMeat();
+    }
 }
